5ca6621e467447c18972165e36c22a35b161f517
henrysky <tianhan_4@aliyun.com>
2025-06-03 22:24:32 +0800

    update deps

--- src/dep.sh
+++ src/dep.sh
@@ -3,4 +3,4 @@ sudo apt-get update ; sudo apt-get install -y cmake libopenmpi-dev zlib1g-dev
 
 # Python
 pip install stable-baselines tensorflow==1.14.0
-pip install stable-baselines[mpi]
+#pip install stable-baselines[mpi]
